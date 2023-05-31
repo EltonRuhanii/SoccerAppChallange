2e9c62a0dc277dcd22fb23f7b73cccc86f4cd97a
Elton Ruhani <134723231+EltonRuhanii@users.noreply.github.com>
2023-05-31 18:51:33 +0200

    Initial Commit

---
+++ SoccerAppChallange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoccerAppChallange
+//
+//  Created by WIZZARD on 31.5.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
